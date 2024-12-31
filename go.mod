ac2ae25d188a8227f0cddbfa50ee125f8d514339
Guo Jin <gitjin@outlook.com>
2024-12-31 20:30:28 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/csguojin/depcalldemod
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,17 @@
+package depcalldemod
+
+import (
+	"fmt"
+)
+
+func MyFunc1() {
+	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+}
+
+func MyFunc2() {
+	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+}
+
+func MyFunc3() {
+	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+}
