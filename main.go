1bb4b680730fc0ca06414da90c77639625cf5f02
Guo Jin <gitjin@outlook.com>
2024-12-31 20:32:28 +0800

    update

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 )
 
 func MyFunc1() {
-	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+	fmt.Println("depcalldemod.MyFunc1, v1.1.0")
 }
 
 func MyFunc2() {
-	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+	fmt.Println("depcalldemod.MyFunc2, v1.1.0")
 }
 
 func MyFunc3() {
-	fmt.Println("depcalldemod.MyFunc1, v1.0.0")
+	fmt.Println("depcalldemod.MyFunc3, v1.1.0")
 }
